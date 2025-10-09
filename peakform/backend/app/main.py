78c8c02fdc41314075cf64ee08bc2fb455190a2e
Andrew White <drewbywhite9@gmail.com>
2025-10-09 15:31:29 -0400

    added initial docker configuration for frontend/backend launch in tandem

--- peakform/backend/app/main.py
+++ peakform/backend/app/main.py
@@ -20,6 +20,9 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "http://frontend:3000",
+        "http://172.18.0.0/16",
+        "*",
     ],  # Next.js frontend
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
